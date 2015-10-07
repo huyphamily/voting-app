244bace67cd7ff6efe39b58c8f4335c7625ff897
huyphamily <huyphamily@gmail.com>
2015-10-07 02:09:20 -0700

    hook up server and store

--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,6 +1,15 @@
 import Server from 'socket.io';
 
 // This creates a Socket.io server, as well as a regular HTTP server bound to port 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
 }
