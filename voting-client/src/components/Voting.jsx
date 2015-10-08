c695753c36db1ec313a8c0a14beecedac18e1681
huyphamily <huyphamily@gmail.com>
2015-10-08 00:09:40 -0700

    change over to react-addons-test-utils and add some test

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -5,10 +5,21 @@ export default class extends React.Component {
     return this.props.pair || [];
   }
 
+  isDisabled() {
+    return !!this.props.hasVoted;
+  }
+
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }
+
   render() {
     let entryButton = this.getPair().map(entry => 
-      <button key={entry}>
+      <button key={entry} onClick={() => this.props.vote(entry)}>
         <h1>{entry}</h1>
+        {this.hasVotedFor(entry) ?
+          <div className="label">Voted</div> :
+          null}
       </button>
     )
     return (
