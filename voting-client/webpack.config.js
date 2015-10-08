f89c7f7fed2c49410dfdc9c30342659830fe560a
huyphamily <huyphamily@gmail.com>
2015-10-07 23:21:38 -0700

    fix typo in webpack config

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,16 +1,18 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.jsx'
   ],
-  module: [{
+  module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel'
-    }]
-  }],
+    }],
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
