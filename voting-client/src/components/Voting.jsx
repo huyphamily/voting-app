0354396471dc4783e337d5fcd77002ca410ec59c
huyphamily <huyphamily@gmail.com>
2015-10-09 01:05:14 -0700

    make all component pure render

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Winner from './Winner';
 import Vote from './Vote';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
   render() {
     return (
       <div>
--- voting-client/src/components/Winner.jsx
+++ voting-client/src/components/Winner.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
   render() {
     return (
       <div ref="winner">Winner is {this.props.winner}!</div>
