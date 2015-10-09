ffe765138210708f8c54dc9a517a5a935a95968b
huyphamily <huyphamily@gmail.com>
2015-10-09 00:08:24 -0700

    add winner logic

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default class extends React.Component {
-  getPair() {
-    return this.props.pair || [];
-  }
-
-  isDisabled() {
-    return !!this.props.hasVoted;
-  }
-
-  hasVotedFor(entry) {
-    return this.props.hasVoted === entry;
-  }
-
   render() {
-    let entryButton = this.getPair().map(entry => 
-      <button key={entry} onClick={() => this.props.vote(entry)}>
-        <h1>{entry}</h1>
-        {this.hasVotedFor(entry) ?
-          <div className="label">Voted</div> :
-          null}
-      </button>
-    )
-    return (
-      <div className="voting">
-        {entryButton}
-      </div>
-    );
+    return (<div>
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />}
+    </div>)
   }
 }
