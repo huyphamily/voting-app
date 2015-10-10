0d43e01bc269707d8f1551831e1c3a431faf4b4c
huyphamily <huyphamily@gmail.com>
2015-10-10 15:42:00 -0700

    add results route and fix App route handler

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -8,7 +8,6 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        {this.props.children}
         {React.cloneElement(this.props.children, {pair: pair })}
       </div>);
   }
--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -3,8 +3,10 @@ import ReactDom from 'react-dom';
 import {Router, Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const routes = <Route path="/" component={App}>
+  <Route path="results" component={Results} />
   <IndexRoute component={Voting} />
 </Route>;
 
