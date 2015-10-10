14387d4a6cf806f7befe72bc489ca05a201310fd
huyphamily <huyphamily@gmail.com>
2015-10-10 16:03:03 -0700

    add tally to results component

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        {React.cloneElement(this.props.children, {pair: pair })}
-      </div>);
+      React.cloneElement(this.props.children, {
+        pair,
+        tally
+      })
+    );
   }
 }
