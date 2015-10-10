b28991c3e9ad0498d906e795425321b0404c5667
huyphamily <huyphamily@gmail.com>
2015-10-10 15:07:32 -0700

    add class name

--- voting-client/src/components/Vote.jsx
+++ voting-client/src/components/Vote.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-export default class extends React.Component {
+export default class Vote extends React.Component {
   constructor(props) {
     super(props);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
